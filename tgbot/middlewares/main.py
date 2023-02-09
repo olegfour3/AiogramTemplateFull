c201e719ce907dac4b67f20832d8f5b528cfcc1d
Oleg Pashuk <45397736+olegfour3@users.noreply.github.com>
2023-02-09 09:41:02 +0300

    правки

--- tgbot/middlewares/main.py
+++ tgbot/middlewares/main.py
@@ -1,11 +1,8 @@
 from tgbot.middlewares.environment import EnvironmentMiddleware
 from tgbot.middlewares.throttling import ThrottlingMiddleware
 
-# from tgbot.middlewares.db import DbMiddleware
-
 
 def register_all_middlewares(dp, config):
     dp.setup_middleware(EnvironmentMiddleware(config=config))
     dp.setup_middleware(ThrottlingMiddleware())
 
-    # dp.setup_middleware(DbMiddleware())
